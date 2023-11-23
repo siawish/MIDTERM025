d26ee996d89ad97bb0de853792797f60cdcb3b20
Ahmed-2022skipq <ahmed.gul.skipq@gmail.com>
2023-11-23 12:29:53 +0500

    Add project files.

---
+++ malik midterm cp lab/Source.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+	string play;
+	int price, product, quantity, s_taxRate,t_s_taxRate;
+	do
+	{
+		float d = 0, t_cost;
+
+		cout << "Enter price of product: ";
+		cin >> price;
+
+		cout << "Enter quantity of product: ";
+		cin >> quantity;
+
+		cout << "Enter salesTax rate of product: (in percentage)";
+		cin >> s_taxRate;
+
+		t_s_taxRate = ((price * (s_taxRate / 100)) + price);
+
+		if (quantity >= 10)
+		{
+			d = (price * 0.10);
+		}
+
+		t_cost = ((price - d) + t_s_taxRate) * quantity;
+
+		cout << "Total cost = " << t_cost << endl;
+
+		cout << "\nDo you want to continue(Y/N): ";
+		cin >> play;
+	}
+	while (play == "Y" || play == "y");
+	return 0;
+}
